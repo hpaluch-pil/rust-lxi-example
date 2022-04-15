41ae64f9e48897b5fba4289f26e2b98fe8317ea0
Henryk Paluch <henryk.paluch@pickering.cz>
2022-04-15 10:23:13 +0200

    Early support for 32-bit MSVC target

--- build.rs
+++ build.rs
@@ -2,11 +2,25 @@
 use std::env;
 
 fn build_windows() {
-    let cb_base_dir = env::var("ClientBridge64").unwrap();
+    let platform_bits = env::var("CARGO_CFG_TARGET_POINTER_WIDTH")
+        .expect("Env CARGO_CFG_TARGET_POINTER_WIDTH is not set");
+    let platform_bits:u32 = platform_bits.parse().unwrap_or_else(|platform_bits|{
+        panic!("Env {}='{}' is not valid u32","CARGO_CFG_TARGET_POINTER_WIDTH",platform_bits);
+    });
+    let cb_env_var = match platform_bits {
+        64 => "ClientBridge64",
+        32 => "ClientBridge",
+        _  => panic!("Unexpected pointer width: {} (variable {})",
+                     platform_bits,"CARGO_CFG_TARGET_POINTER_WIDTH"),
+    };
+
+    let cb_base_dir = env::var(cb_env_var).unwrap_or_else(|cb_env_var|{
+        panic!("Env Variable '{}' not set. Ensure that you have ClientBridge/C++ installed.",cb_env_var);
+    });
     let cb_lib_path = format!("{}\\Lib\\MSC",cb_base_dir);
     // this magic is required to link picmlx_w64.lib
-    println!("cargo:rustc-link-lib=picmlx_w64");
-    println!("cargo:rustc-link-lib=piplx_w64");
+    println!("cargo:rustc-link-lib=picmlx_w{}",platform_bits);
+    println!("cargo:rustc-link-lib=piplx_w{}",platform_bits);
     println!("cargo:rustc-link-search=native={}",cb_lib_path);
 }
 
